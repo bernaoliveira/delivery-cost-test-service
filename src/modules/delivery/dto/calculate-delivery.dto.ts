6df08ac39f7715b323bf4d1428deb2956faaa087
Danil Gilman <d.gilman@skycoach.gg>
2026-07-17 15:22:12 +0200

    fix(delivery): validation fix

--- src/modules/delivery/dto/calculate-delivery.dto.ts
+++ src/modules/delivery/dto/calculate-delivery.dto.ts
@@ -1,4 +1,4 @@
-import { ValidateNested } from 'class-validator';
+import { IsNotEmpty, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { BasketDto } from '../../../common/dto/basket.dto';
 import { AddressDto } from '../../../common/dto/address.dto';
@@ -6,9 +6,11 @@ import { AddressDto } from '../../../common/dto/address.dto';
 export class CalculateDeliveryDto {
   @ValidateNested()
   @Type(() => BasketDto)
+  @IsNotEmpty()
   basket: BasketDto;
 
   @ValidateNested()
   @Type(() => AddressDto)
+  @IsNotEmpty()
   address: AddressDto;
 }
